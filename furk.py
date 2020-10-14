81876026f29cdaedbe777458cde88f69d0d613ba
justcop <justcop@gmail.com>
2020-10-14 20:10:58 +0100

    Release candidate
    Release candidate

---
+++ furk.py
@@ -0,0 +1,127 @@
+#!/usr/local/bin/python3
+
+import os
+import glob
+import requests
+import json
+import ast
+import logging
+import urllib
+import time
+
+from guessit import guessit
+from bs4 import BeautifulSoup
+from torrentool.api import Torrent
+
+from configs import furk_api
+from configs import torrents_path
+from configs import completed_path
+from configs import TV_path
+from configs import sonarr_key
+from configs import sonarr_address
+
+logging.basicConfig(handlers=[logging.FileHandler("/users/justcop/furk.log"),logging.StreamHandler()],format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+
+sonarr_url = sonarr_address + '/api/{}?apikey=' + sonarr_key
+
+timeout = 0
+
+while True:
+
+    processed = 0
+    retry = 0
+
+    for filename in glob.glob(os.path.join(torrents_path, '*.torrent')):
+      torrent = Torrent.from_file(filename)
+      with open(filename + ".magnet", 'w') as f:
+       f.write(torrent.magnet_link)
+      os.remove(filename)
+
+
+    for filename in glob.glob(os.path.join(torrents_path, '*.magnet')):
+      with open(filename, 'r') as f:
+        magnet = f.read()
+
+        try:
+            base_url = 'https://www.furk.net/api/dl/add?url={}&api_key={}'
+            data = (requests.get(base_url.format(magnet,furk_api))).json()
+        except:
+            logging.error("Unable to get valid furk response for this torrent.")
+            logging.error(data)
+            continue
+
+        try:
+         files = data["files"][0]
+         logging.info("Torrent processing"+data["files"][0]["name"])
+        except:
+            try:
+                if data["torrent"]["dl_status"] == "active" or "finished":
+                    pass
+            except:
+                logging.error("furk returned unexpected response, without file date")
+                logging.error(str(data))
+                continue
+            else:
+                logging.warning("furk file is not yet ready for download")
+                retry += 1
+        else:
+          try:
+            xspfurl = urllib.request.urlopen(files["url_pls"])
+          except:
+            logging.warning("furk file is not yet ready for download")
+            retry += 1
+          else:
+            xspf = xspfurl.read()
+            soup = BeautifulSoup(xspf, "lxml")
+            title = soup('title')
+            strmurl = soup('location')
+
+            try:
+                for x in range(len(strmurl)):
+                    try:
+                        metadata = guessit(str(title[x+1].text))
+                        path = completed_path + '/' + str(metadata.get('title')) + ' - ' + 'S' + str(metadata.get('season')) + "E" + str(metadata.get('episode')) + ' - [' + str(metadata.get('source')) + '-' + str(metadata.get('screen_size')) + ']'
+                        episode = str(metadata.get('title')) + ' - ' + 'S' + str(metadata.get('season')) + 'E' + str(metadata.get('episode')) + ' - [' + str(metadata.get('source')) + ' - ' + str(metadata.get('screen_size')) + ']'
+                        if len(strmurl) > 1:
+                            logging.info("Episode processing " + episode)
+                        try:
+                            os.mkdir(path)
+                        except FileExistsError:
+                            pass
+                        strm = open(path+'/'+ episode +'.strm', 'w')
+                        strm.write(strmurl[x].string)
+                        strm.close()
+                        processed += 1
+                    except Exception as e:
+                        logging.error("Unable to write a valid strm file")
+                        logging.error(e)
+            except Exception as e:
+                logging.error("Unable to process entire strm file")
+                logging.error(e)
+            else:
+                try:
+                    logging.info("Completed processing "+data["files"][0]["name"]+"/n")
+                    os.remove(filename)
+                    data = {'name':'DownloadedEpisodesScan','path':path}
+                    response = (requests.post(sonarr_url.format('command'),json=data)).json()
+                except:
+                    try:
+                        logging.info(response['body']['completionMessage'])
+                    finally:
+                        logging.warning("Unable to update sonarr")
+
+    logging.info(str(processed) + " files have been processed.")
+
+    if timeout == 6:
+        logging.info("Timed-out, will check again during next scheduled run")
+        break
+    elif retry > 0:
+        timeout += 1
+        logging.info("Repeating check for remaining episodes in 10 minutes, allowing time for Furk to download")
+        time.sleep(600)
+    else:
+        break
---
+++ linker.py
@@ -0,0 +1,105 @@
+import urllib.request, urllib.error, urllib.parse
+import os
+import pickle
+import datetime
+import requests
+import logging
+
+from pathlib import Path
+from dateutil import parser
+from guessit import guessit
+
+from configs import TV_path
+from configs import sonarr_key
+from configs import sonarr_address
+
+logging.basicConfig(handlers=[logging.FileHandler("/users/justcop/furk.log"),logging.StreamHandler()],format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+flaggedforremoval = {}
+flaggedlist = []
+
+sonarr_url = sonarr_address + '/api/{}?apikey=' + sonarr_key
+
+
+try:
+ oldflaggedforremoval = pickle.load(open("flaggedforremoval.pkl", 'rb'))
+except:
+ pass
+
+try:
+    r = urllib.request.urlopen("https://www.furk.net/")
+except urllib.error.URLError as e:
+    r = e
+if r.code in (200, 401):
+ for filename in Path(TV_path).rglob('*.strm'):
+  with open(filename, 'r') as f:
+    url = f.read()
+    f.close()
+    try:
+        r = urllib.request.urlopen(url)
+    except urllib.error.URLError as e:
+        r = e
+    if r.code in (200, 401):
+        continue
+    else:
+        filename = str(filename)
+        time = str(datetime.datetime.now())
+        try:
+         oldtime = oldflaggedforremoval[filename]['time']
+         logging.info("File " + (filename.rsplit("/")[-1]) + " has already been flagged for removal at " + oldtime)
+         flaggedforremoval[filename] = {}
+         flaggedforremoval[filename]['time'] = oldtime
+        except:
+         logging.info("Flagging " + (filename.rsplit("/")[-1]) + " for removal as stream URL not currently valid. Will check again in 24 hours and remove if still not available")
+         flaggedforremoval[filename] = {}
+         flaggedforremoval[filename]['time'] = time
+
+ for filename in flaggedforremoval:
+    logging.info("Checking if current file " + (filename.rsplit("/")[-1]) + " has been flagged for more than 24 hours")
+    timeflagged = (flaggedforremoval[filename]['time'])
+    logging.info("Flagged at " + timeflagged)
+    elapsed = datetime.datetime.now() - parser.parse(timeflagged)
+    if elapsed > datetime.timedelta(hours=0):
+        flaggedlist.append(filename)
+        logging.info("... It Has")
+    else:
+        logging.info("... It Hasn't")
+
+ for filename in flaggedlist:
+    logging.info("Deleting" + (filename.rsplit("/")[-1]))
+    os.remove(filename)
+    flaggedforremoval.pop(filename)
+    title = filedict.get('title')
+    seasonNumber = filedict.get('season')
+    episodeNumber = filedict.get('episode')
+
+
+
+    series = requests.get(sonarr_url.format('series'))
+    series = series.json()
+    for x in series:
+        if x["title"] == title:
+            seriesId = x["id"]
+            break
+    data = {'name':'rescanSeries','seriesId': seriesId}
+    requests.post(sonarr_url.format('command'),json=data)
+    episodes = requests.get(sonarr_url.format('episode'), params={'SeriesiD':seriesId})
+    episodes = episodes.json()
+    for data in episodes:
+        if data['seasonNumber'] == seasonNumber and data['episodeNumber'] == episodeNumber:
+            data['monitored']=False
+            data = str(json.dumps(data))
+            break
+
+    requests.put(sonarr_url.format('episode'), data=data, headers = {"Content-Type": "application/json"})
+    requests.get(sonarr_url.format('wanted/missing'), data=data, headers = {"Content-Type": "application/json"})
+
+
+ f = open("flaggedforremoval.pkl","wb")
+ pickle.dump(flaggedforremoval,f)
+ f.close()
+
+else:
+    logging.error("Unable to connect to Furk. Is computer connected to internet, or furk down?")
