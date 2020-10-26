09e77539280a0f3838fe73320856fc1691fa0514
justcop <justcop@gmail.com>
2020-10-26 02:03:39 +0000

    Update furk.py

--- furk.py
+++ furk.py
@@ -115,13 +115,3 @@ while True:
                         logging.warning("Unable to update sonarr")
 
     logging.info(str(processed) + " files have been processed.")
-
-#    if timeout == 6:
-#        logging.info("Timed-out, will check again during next scheduled run")
-#        break
-#    elif retry > 0:
-#        timeout += 1
-#        logging.info("Repeating check for remaining episodes in 10 minutes, allowing time for Furk to download")
-#        time.sleep(600)
-#    else:
-#        break
