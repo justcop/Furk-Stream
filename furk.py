89eb0ab2c2eb07a21acbf091201aa3e547db98b4
justcop <justcop@gmail.com>
2020-10-26 02:54:18 +0000

    Update furk.py

--- furk.py
+++ furk.py
@@ -20,7 +20,7 @@ from configs import TV_path
 from configs import sonarr_key
 from configs import sonarr_address
 
-logging.basicConfig(handlers=[logging.FileHandler("/config/home-assistant.log"),logging.StreamHandler()],format='%(asctime)s %(levelname)-8s (Furk) %(message)s',
+logging.basicConfig(handlers=[logging.FileHandler("/config/home-assistant.log"),logging.StreamHandler()],format='%(asctime)s %(levelname)-8s(Furk)%(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
