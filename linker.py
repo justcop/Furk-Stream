25c22e5e8f06bfaeaf622dd9a572786d441dccca
justcop <justcop@gmail.com>
2021-01-24 01:32:28 +0000

    Update linker.py

--- linker.py
+++ linker.py
@@ -48,7 +48,7 @@ except:
  pass
 
 logging.info("Checking integrity of any strm files currently in library")
-for filename in in os.listdir(media_path):
+for filename in os.listdir(media_path):
  logging.info("." + filename)
  with open(filename, 'r') as f:
     url = f.read()
