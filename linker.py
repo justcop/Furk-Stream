b50ac83f0bf3602932971f5aaafcafbc3b35f9ff
justcop <justcop@gmail.com>
2021-01-24 11:08:52 +0000

    Update linker.py

--- linker.py
+++ linker.py
@@ -54,7 +54,8 @@ for filename in Path(media_path).rglob('*.strm'):
     url = f.read()
     f.close()
     logging.info(str(filename))
-    r = json.loads(headers(requests.head(url)))
+    r = requests.head(url))
+    r = json.loads(r.headers)
     logging.info(str(r))
     #encoding = r.info().get_content_charset('utf8')
     
