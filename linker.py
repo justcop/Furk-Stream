37bf447f68c5ec4ef82a0ae74547a2cf8737e7f1
justcop <justcop@gmail.com>
2021-01-24 11:10:48 +0000

    Update linker.py

--- linker.py
+++ linker.py
@@ -55,11 +55,10 @@ for filename in Path(media_path).rglob('*.strm'):
     f.close()
     logging.info(str(filename))
     r = requests.head(url)
-    r = json.loads(r.headers)
     logging.info(str(r))
     #encoding = r.info().get_content_charset('utf8')
     
-    if r['Warning'] == 'file_not_found':
+    if r.headers['warning'] == 'file_not_found':
         filename = str(filename)
         time = str(datetime.datetime.now())
         try:
