663c7a2dbfab91bd7930b81c7bda167cbe8ff789
justcop <justcop@gmail.com>
2021-08-09 12:26:13 +0100

    Update linker.py

--- linker.py
+++ linker.py
@@ -22,9 +22,11 @@ from configs import sonarr_address
 from configs import completed_path
 from configs import torrents_path
 
+Try:
+ logging.basicConfig(handlers=[logging.FileHandler("/config/home-assistant.log"),TimedRotatingFileHandler(os.path.dirname("furk.log"), when="midnight", interval=1, backupCount=7),logging.StreamHandler()],format='%(asctime)s %(levelname)s (Furk Link-Check) %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+Except:
+ logging.basicConfig(handlers=[TimedRotatingFileHandler(os.path.dirname("furk.log"), when="midnight", interval=1, backupCount=7),logging.StreamHandler()],format='%(asctime)s %(levelname)s (Furk Link-Check) %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
-#logging.basicConfig(handlers=[logging.FileHandler("/config/home-assistant.log"),TimedRotatingFileHandler("/config/furk.log", when="midnight", interval=1, backupCount=7),logging.StreamHandler()],format='%(asctime)s %(levelname)s (Furk Link-Check) %(message)s',
-logging.basicConfig(handlers=[TimedRotatingFileHandler(os.path.dirname("furk.log"), when="midnight", interval=1, backupCount=7),logging.StreamHandler()],format='%(asctime)s %(levelname)s (Furk Link-Check) %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 flagged = {}
 removing = []
