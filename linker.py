ac5007930f16e4f3cc661b0128f21464db462ef3
justcop <justcop@gmail.com>
2021-08-09 15:01:21 +0100

    Update linker.py

--- linker.py
+++ linker.py
@@ -70,7 +70,7 @@ for filename in strmfiles:
     f = str(filename) 
     try: #checks if furk gives a file not found error
       if r.headers['warning'] == 'file_not_found':
-        logging.info("Deleting expired stream" + f.rsplit("/")[-1]) 
+        logging.info("Deleting expired stream " + f.rsplit("/")[-1]) 
         os.remove(filename)
         show = guessit(filename)
         title = show.get('title')
