ca6ad088f9348db88104047d1cc2d8ed1d3c8f00
justcop <justcop@gmail.com>
2021-11-04 14:20:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
+
 echo "installing python virtual environment"
 python3 -m venv env
 source env/bin/activate
+
+echo "installing python packages in virtual environment"
 pip install -r requirements.txt
 deactivate
 set –o noclobber
+
+echo "Creating scripts to launch in virtual environment"
+
 echo "./env/bin/python3" furk.py > furk.sh
 echo "./env/bin/python3" linker.py > linker.sh
 echo "./env/bin/python3" strmFromFurkURL.py > strmFromFurkURL.sh
