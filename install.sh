2561288fd58e8944b907176bafdac234220cc51f
justcop <justcop@gmail.com>
2021-11-04 13:51:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ source env/bin/activate
 echo ‘env' > .gitignore
 pip install -r requirements.txt
 deactivate
+set –o noclobber
+echo "./env/bin/python3" furk.py > furk.sh
+echo "./env/bin/python3" linker.py > linker.sh
+echo "./env/bin/python3" strmFromFurkURL.py > strmFromFurkURL.sh
+set +o noclobber
