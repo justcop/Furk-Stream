f2cd878a3ae096ad35e47d689373d8589df57e78
justcop <justcop@gmail.com>
2021-11-06 02:36:40 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,18 +11,28 @@ set –o noclobber
 
 echo "Creating scripts to launch in virtual environment"
 
-echo "#!/bin/bash                                                                                   
-parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
-cd \"$parent_path\"                                                                              
-/env/bin/python3 furk.py" > furk.sh
-echo "#!/bin/bash                                                                                   
-parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
-cd \"$parent_path\"                                                                              
-/env/bin/python3 linker.sh" > linker.sh
-echo "#!/bin/bash                                                                                   
-parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
-cd \"$parent_path\"                                                                              
-/env/bin/python3 strmFromFurkURL.sh" > strmFromFurkURL.sh
+cat<<END > furk.sh
+#!/bin/bash                                                                                 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )                                
+cd "$parent_path"                                                                              
+./env/bin/python3 furk.py
+END
+
+cat<<END > linker.sh
+#!/bin/bash                                                                                 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )                                
+cd "$parent_path"                                                                              
+./env/bin/python3 linker.py
+END
+
+
+cat<<END > strmFromFurkURL.sh
+#!/bin/bash                                                                                 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )                                
+cd "$parent_path"                                                                              
+./env/bin/python3 strmFromFurkURL.py
+END
+
 chmod +x furk.sh
 chmod +x linker.sh
 chmod +x strmFromFurkURL.sh
