a521f22116adee547e322e294d3cc49344393935
justcop <justcop@gmail.com>
2021-11-06 02:16:52 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,15 +14,15 @@ echo "Creating scripts to launch in virtual environment"
 echo "#!/bin/bash                                                                                   
 parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
 cd \"$parent_path\"                                                                              
-./env/bin/python3 furk.py" > furk.sh
+/env/bin/python3 furk.py" > furk.sh
 echo "#!/bin/bash                                                                                   
 parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
 cd \"$parent_path\"                                                                              
-./env/bin/python3 linker.sh" > linker.sh
+/env/bin/python3 linker.sh" > linker.sh
 echo "#!/bin/bash                                                                                   
 parent_path=$( cd \"$(dirname \"${BASH_SOURCE[0]}\")\" ; pwd -P )                                  
 cd \"$parent_path\"                                                                              
-./env/bin/python3 strmFromFurkURL.sh" > strmFromFurkURL.sh
+/env/bin/python3 strmFromFurkURL.sh" > strmFromFurkURL.sh
 chmod +x furk.sh
 chmod +x linker.sh
 chmod +x strmFromFurkURL.sh
