6bd5bd7c83f1527a502ef050bd8aab080b024485
justcop <justcop@gmail.com>
2021-11-08 23:12:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
  
 echo "installing python virtual environment"
-#python3 -m venv env
-#source env/bin/activate
+
+python3 -m venv env
+source env/bin/activate
 
 echo "installing python packages in virtual environment"
-#pip install -r requirements.txt
+pip install -r requirements.txt
 deactivate
 
 echo "Creating scripts to launch in virtual environment"
 
 for f in *.py
 do
+if [[ $f == "configs.py" ]]; then
+continue
+fi
 parent_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "#! /bin/sh
 $parent_path/env/bin/python3 furk.py
