8d64c2e4b0a6c54700bc3031b6d710413c7c33f7
justcop <justcop@gmail.com>
2021-11-09 01:28:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
- 
+
+parent_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) 
 echo "installing python virtual environment"
 
 python3 -m venv env
 source env/bin/activate
 
 echo "installing python packages in virtual environment"
-pip install -r requirements.txt
+pip install -r 
+$parent_path/requirements.txt
 deactivate
 
 echo "Creating scripts to launch in virtual environment"
@@ -16,11 +18,10 @@ do
 if [[ $f == "configs.py" ]]; then
 continue
 fi
-parent_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 echo "#! /bin/sh
 source $parent_path/env/bin/activate
 $parent_path/$f \$1 \$2 \$3
-" > ${f::-2}sh
+" > $parent_path/${f::-2}sh
 done
 
 chmod +x *.sh
